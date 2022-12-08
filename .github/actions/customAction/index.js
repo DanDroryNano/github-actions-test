f5e4fa3854a91131d89d41793f030a93edab6768
Dan Drory <dan.drory@nano.com.au>
2022-12-08 12:26:09 +1100

    update custom number

--- .github/actions/customAction/index.js
+++ .github/actions/customAction/index.js
@@ -3,7 +3,9 @@ const core = require('@actions/core');
 try {
   const listOfNumbers = core.getInput('list-of-numbers');
   console.log(`Supplied numbers ${listOfNumbers}!`);
-  core.setOutput("number", listOfNumbers);
+  const numbers = listOfNumbers.split(',').map(Number);
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  core.setOutput("number", randomNumber);
 } catch (error) {
   core.setFailed(error.message);
 }
